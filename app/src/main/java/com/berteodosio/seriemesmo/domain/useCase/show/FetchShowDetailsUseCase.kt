8685711c91327523a7841219c748a9cdf64c7728
Bernardo do Amaral Teodosio <berteodosio@gmail.com>
2021-04-30 23:33:27 -0300

    Removing Special seasons

--- app/src/main/java/com/berteodosio/seriemesmo/domain/useCase/show/FetchShowDetailsUseCase.kt
+++ app/src/main/java/com/berteodosio/seriemesmo/domain/useCase/show/FetchShowDetailsUseCase.kt
@@ -1,5 +1,6 @@
 package com.berteodosio.seriemesmo.domain.useCase.show
 
+import com.berteodosio.seriemesmo.data.tmdb.model.TmdbShow
 import com.berteodosio.seriemesmo.data.tmdb.repository.TmdbRepository
 import com.berteodosio.seriemesmo.domain.useCase.base.BaseUseCase
 import com.berteodosio.seriemesmo.domain.mapper.toShow
@@ -11,7 +12,12 @@ class FetchShowDetailsUseCase(private val tmdbRepository: TmdbRepository) : Base
     fun execute(showId: Long): Single<Show> {
         return tmdbRepository
             .fetchShowDetails(showId)
+            .map(::removeSpecialSeason)
             .map { it.toShow() }
+    }
+
+    private fun removeSpecialSeason(show: TmdbShow): TmdbShow {
+        return show.apply { seasons = seasons?.filter { it.name.equals("specials", ignoreCase = true).not() } }
     }
 
 }
